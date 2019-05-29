d78297fe5ced5f3a9af0fda3dc9dc46bd9043834
Aaron B <abriscoe2008@gmail.com>
2019-05-29 20:54:21 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,14 +1,9 @@
-def my_collect(array) # put argument(s) here
-  # code here
+def my_collect(array)
   i = 0
-  result = []
-
-  while i < array.size
-    block_given? # optional
-    result << yield(array[i]) # if block, then yield to block # also need to pass argument(s) to yield, otherwise block is operating on nil
-    i += 1
+  collect = []
+  while i < array.length
+    collect << yield(array[i])
+    i+=1
   end
-result
+  collect
 end
-  
-
